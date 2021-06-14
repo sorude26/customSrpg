e9bab25d09710927ca79d1a02d7e6d0e22b87fda
sorude26 <sorude26@gmail.com>
2021-06-14 16:02:16 +0900

    パーツ関係の基底クラスを追加した

---
+++ customSrpg/Assets/Script/PartsMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsMaster : MonoBehaviour
+{
+    /// <summary> パーツID </summary>
+    [SerializeField] int m_partsID;
+    /// <summary> パーツID </summary>
+    public int PartsID { get => m_partsID; }
+    /// <summary> パーツ名 </summary>
+    [SerializeField] string m_partsName;
+    /// <summary> パーツ名 </summary>
+    public string PartsName { get => m_partsName; }
+    /// <summary> 重量 </summary>
+    [SerializeField] protected int m_weight = 10;
+    /// <summary> 重量 </summary>
+    public int Weight { get => m_weight; }
+    /// <summary> パーツサイズ </summary>
+    [SerializeField] int m_partsSize = 1;
+    /// <summary> パーツサイズ </summary>
+    public int PartsSize { get => m_partsSize; }
+
+    /// <summary> 命中精度 </summary>
+    [SerializeField] protected int m_hitAccuracy;
+    /// <summary> 命中精度 </summary>
+    public int HitAccuracy { get => m_hitAccuracy; }
+}
---
+++ customSrpg/Assets/Script/UnitPartsMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPartsMaster : PartsMaster
+{
+    /// <summary> パーツ耐久値 </summary>
+    [SerializeField] protected int m_partsHp;
+    /// <summary> パーツ耐久値 </summary>
+    public int MaxPartsHp { get => m_partsHp; }
+    /// <summary> パーツ装甲値 </summary>
+    [SerializeField] protected int m_defense;
+    /// <summary> パーツ装甲値 </summary>
+    public int Defense { get => m_defense; }
+    /// <summary> 現在のパーツ耐久値 </summary>
+    public int CurrentPartsHp { get; protected set; }
+
+    /// <summary>
+    /// パーツの初期化
+    /// </summary>
+    protected virtual void StartSet()
+    {
+        CurrentPartsHp = m_partsHp;
+    }
+}
