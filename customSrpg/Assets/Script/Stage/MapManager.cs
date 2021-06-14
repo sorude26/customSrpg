d0629442aa5e5930d96f023d541af4eda30d5401
sorude26 <sorude26@gmail.com>
2021-06-14 16:30:43 +0900

    マップ生成用スクリプトを追加した

--- customSrpg/Assets/Script/Stage/MapManager.cs
+++ customSrpg/Assets/Script/Stage/MapManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MapManager Instance { get; private set; }
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instance = this;
     }
 }
