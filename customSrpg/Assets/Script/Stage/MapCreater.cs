a5d234b0bbdb787818d003628656508f7ef3c143
sorude26 <sorude26@gmail.com>
2021-06-14 17:09:47 +0900

    地形用仮Prefabと地形用Materialを追加し、生成テストを行った

--- customSrpg/Assets/Script/Stage/MapCreater.cs
+++ customSrpg/Assets/Script/Stage/MapCreater.cs
@@ -13,60 +13,62 @@ public enum MapType
     Wasteland,//荒地
     Forest,//森
 }
+ /// <summary>
+ /// 地形データ
+ /// </summary>
+public class MapData
+{
+    /// <summary> X座標 </summary>
+    public int PosX { get; private set; }
+    /// <summary> Z座標 </summary>
+    public int PosZ { get; private set; }
+    /// <summary> 高さ </summary>
+    public float Level { get; private set; }
+    /// <summary> 地形種 </summary>
+    public MapType MapType { get; private set; }
+    /// <summary> 移動計算用データ </summary>
+    public int MovePoint { get; set; }
+    /// <summary>
+    /// 初期設定
+    /// </summary>
+    /// <param name="mapType"></param>
+    /// <param name="posX"></param>
+    /// <param name="posZ"></param>
+    /// <param name="level"></param>
+    public MapData(MapType mapType, int posX, int posZ, float level)
+    {
+        MapType = mapType;
+        PosX = posX;
+        PosZ = posZ;
+        Level = level;
+        MovePoint = 0;
+    }
+}
 public class MapCreater : MonoBehaviour
 {
     /// <summary> 地形に合わせたオブジェクトのPrefab </summary>
     [SerializeField] GameObject[] m_stagePrefabs;
-
-    /// <summary>
-    /// 地形データ
-    /// </summary>
-    public class MapDate
-    {
-        /// <summary> X座標 </summary>
-        public int PosX { get; private set; }
-        /// <summary> Z座標 </summary>
-        public int PosZ { get; private set; }
-        /// <summary> 高さ </summary>
-        public float Level { get; private set; }
-        /// <summary> 地形種 </summary>
-        public MapType MapType { get; private set; }
-        /// <summary> 移動計算用データ </summary>
-        public int MovePoint { get; set; }
-        /// <summary>
-        /// 初期設定
-        /// </summary>
-        /// <param name="mapType"></param>
-        /// <param name="posX"></param>
-        /// <param name="posZ"></param>
-        /// <param name="level"></param>
-        public MapDate(MapType mapType, int posX, int posZ, float level)
-        {
-            MapType = mapType;
-            PosX = posX;
-            PosZ = posZ;
-            Level = level;
-            MovePoint = 0;
-        }
-    }
+    /// <summary> 地形サイズ </summary>
+    [SerializeField] int m_mapScale = 10;
+    public int MapScale { get => m_mapScale; }
     /// <summary>
     /// 平らな地形を生成しデータを返す
     /// </summary>
     /// <param name="maxX"></param>
     /// <param name="maxZ"></param>
     /// <returns></returns>
-    public MapDate[] MapCreate(int maxX, int maxZ)
+    public MapData[] MapCreate(int maxX, int maxZ,Transform parent)
     {
-        MapDate[] mapDates = new MapDate[maxX * maxZ];
+        MapData[] mapDates = new MapData[maxX * maxZ];
         for (int i = 0; i < maxZ; i++)
         {
             for (int j = 0; j < maxX; j++)
             {
-                MapDate map = new(MapType.Normal, j, i, 0);
+                MapData map = new MapData(MapType.Normal, j, i, 0);
                 mapDates[j + i * maxX] = map;
                 GameObject mapPanel = Instantiate(m_stagePrefabs[0]);
-                mapPanel.transform.position = new Vector3(j * 10, 0, i * 10);
-                mapPanel.transform.SetParent(transform);
+                mapPanel.transform.position = new Vector3(j * MapScale, 0, i * MapScale);
+                mapPanel.transform.SetParent(parent);
             }
         }
         return mapDates;
--- customSrpg/Assets/Script/Stage/MapManager.cs
+++ customSrpg/Assets/Script/Stage/MapManager.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public static MapManager Instance { get; private set; }
+    /// <summary>  </summary>
+    [SerializeField] MapCreater m_mapCreater;
+    /// <summary> マップの全情報 </summary>
+    public MapData[] MapDatas { get; private set; }
+    /// <summary> 移動範囲 </summary>
+    public List<MapData> MoveList { get; private set; }
+    /// <summary> ステージの最大X座標 </summary>
+    [SerializeField] int m_maxX = 15;
+    /// <summary> ステージの最大Z座標 </summary>
+    [SerializeField] int m_maxZ = 15;
+    /// <summary> ステージの最大X座標 </summary>
+    public int MaxX { get => m_maxX; }
+    /// <summary> ステージの最大Z座標 </summary>
+    public int MaxZ { get => m_maxZ; }
     private void Awake()
     {
         Instance = this;
+    }
+    private void Start()
+    {
+        MapDatas = m_mapCreater.MapCreate(m_maxX, m_maxZ,this.transform);
+        MoveList = new List<MapData>();
     }
 }
