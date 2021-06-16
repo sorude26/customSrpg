35399cee1e9b94bd1a09606269648dbaeb2d5a9d
sorude26 <sorude26@gmail.com>
2021-06-16 20:00:20 +0900

    ステージパネルに座標を持たせた

--- customSrpg/Assets/Script/Stage/StagePanel.cs
+++ customSrpg/Assets/Script/Stage/StagePanel.cs
@@ -8,14 +8,14 @@ public class StagePanel : MonoBehaviour
     [SerializeField] GameObject m_movePanel;
     /// <summary> 攻撃可能表示用パネル </summary>
     [SerializeField] GameObject m_attackPanel;
-    private void OnEnable()
+    bool m_moveMode;
+    bool m_attackMode;
+    int m_posX;
+    int m_posZ;
+    public void SetPos(int x,int z)
     {
-        ViewEnd();
-        EventManager.OnStageGuideViewEnd += ViewEnd;
-    }
-    private void OnDisable()
-    {
-        EventManager.OnStageGuideViewEnd -= ViewEnd;
+        m_posX = x;
+        m_posZ = z;
     }
     /// <summary>
     /// 移動可能表示を出す
@@ -23,6 +23,7 @@ public class StagePanel : MonoBehaviour
     public void ViewMovePanel()
     {
         m_movePanel.SetActive(true);
+        m_moveMode = true;
     }
     /// <summary>
     /// 攻撃可能表示を出す
@@ -30,6 +31,15 @@ public class StagePanel : MonoBehaviour
     public void ViewAttackPanel()
     {
         m_attackPanel.SetActive(true);
+        m_attackMode = true;
+    }
+    /// <summary>
+    /// 攻撃可能表示を消す
+    /// </summary>
+    public void CloseAttackPanel()
+    {
+        m_attackPanel.SetActive(false);
+        m_attackMode = false;
     }
     /// <summary>
     /// 表示パネルを閉じる
@@ -38,5 +48,27 @@ public class StagePanel : MonoBehaviour
     {
         m_movePanel.SetActive(false);
         m_attackPanel.SetActive(false);
+        m_moveMode = false;
+        m_attackMode = false;
+    }
+    private void OnMouseDown()
+    {
+        if (m_attackMode)
+        {
+            return;
+        }
+        if (m_moveMode)
+        {
+            StageManager.Instance.PointMoveTest(m_posX, m_posZ);
+        }
+    }
+    private void OnEnable()
+    {
+        ViewEnd();
+        EventManager.OnStageGuideViewEnd += ViewEnd;
+    }
+    private void OnDisable()
+    {
+        EventManager.OnStageGuideViewEnd -= ViewEnd;
     }
 }
