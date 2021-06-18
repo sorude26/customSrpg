bb5df719572192ec53fbdc53d6764013ef55dfa1
sorude26 <sorude26@gmail.com>
2021-06-18 20:03:52 +0900

    BattleManagerを追加した

---
+++ customSrpg/Assets/Script/Stage/BattleManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// 戦闘処理担当
+/// </summary>
+public class BattleManager : MonoBehaviour
+{
+    List<Unit> m_attackTarget;
+    public void AttackTarget()
+    {
+        m_attackTarget = new List<Unit>();
+        var targetPos = StageManager.Instance.GetAttackTarget();
+        var targetUnit = StageManager.Instance.GetStageUnits();
+        foreach (var unit in targetUnit)
+        {
+            var t = targetPos.Where(px => px.PosX == unit.CurrentPosX).Where(pz => pz.PosZ == unit.CurrentPosZ).FirstOrDefault();
+            if(t != null)
+            {
+                m_attackTarget.Add(unit);
+            }
+        }
+        foreach (var item in m_attackTarget)
+        {
+            Debug.Log(item);
+        }
+    }
+}
