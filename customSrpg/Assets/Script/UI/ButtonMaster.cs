e7006c241d580e0f12ae812448d2f33b86c7dcbc
sorude26 <sorude26@gmail.com>
2021-07-03 19:40:58 +0900

    UIのボタンを操作するスクリプトを追加した

---
+++ customSrpg/Assets/Script/UI/ButtonMaster.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// UIのボタンを操作する
+/// </summary>
+public class ButtonMaster : MonoBehaviour
+{
+    [Tooltip("選択位置の表示")]
+    [SerializeField] protected RectTransform m_targetMark;
+    [Tooltip("選択肢")]
+    [SerializeField] protected Button[] m_buttons;
+    /// <summary> 選択位置番号 </summary>
+    protected int m_buttonNum = 0;
+    /// <summary>
+    /// 選択肢を開く
+    /// </summary>
+    public virtual void Open()
+    {
+        gameObject.SetActive(true);
+        m_buttonNum = 0;
+        CursorMove();
+    }
+    /// <summary>
+    /// 選択肢を閉じる
+    /// </summary>
+    public virtual void Close()
+    {
+        gameObject.SetActive(false);
+    }
+    /// <summary>
+    /// カーネル位置を上に移動する
+    /// </summary>
+    public void CursorUp()
+    {
+        if (m_buttonNum > 0)
+        {
+            m_buttonNum--;
+            CursorMove();
+        }
+    }
+    /// <summary>
+    /// カーネル位置を下に移動する
+    /// </summary>
+    public void CursorDown()
+    {
+        if (m_buttonNum < m_buttons.Length - 1)
+        {
+            m_buttonNum++;
+            CursorMove();
+        }
+    }
+    /// <summary>
+    /// カーソルを移動させる
+    /// </summary>
+    protected virtual void CursorMove()
+    {
+        m_targetMark.transform.localPosition = m_buttons[m_buttonNum].transform.localPosition;
+    }
+    /// <summary>
+    /// ボタンを押す
+    /// </summary>
+    public virtual void Decision()
+    {
+        m_buttons[m_buttonNum].onClick.Invoke();
+    }
+}
---
+++ customSrpg/Assets/Script/UI/WeaponButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponButtons : ButtonMaster
+{
+    [SerializeField] GameObject[] m_buttonGuard;
+    public override void Open()
+    {
+        base.Open();
+       
+    }
+    protected override void CursorMove()
+    {
+        base.CursorMove();
+        if (m_buttonNum < (int)WeaponPosition.None)
+        {
+            if (StageManager.Instance.TurnUnit.GetUnitData().GetWeapon((WeaponPosition)m_buttonNum) != null)
+            {
+                StageManager.Instance.AttackSearch((WeaponPosition)m_buttonNum);
+            }
+        }
+    }
+}
