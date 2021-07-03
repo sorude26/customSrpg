81ab6af1bbfbece536ffcc61ff5eae1a3a4ee7dc
sorude26 <sorude26@gmail.com>
2021-07-03 19:44:37 +0900

    InputManagerに方向キー入力用と決定キー入手用イベントを追加した

--- customSrpg/Assets/Script/InputManager.cs
+++ customSrpg/Assets/Script/InputManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +8,25 @@ namespace Stage
     public class InputManager : MonoBehaviour
     {
         public static InputManager Instance { get; private set; }
+        public event Action<float, float> OnInputArrow;
+        public event Action OnInputDecision;
         private void Awake()
         {
             Instance = this;
         }
         void Update()
         {
-
+            if (Input.GetButtonDown("Jump"))
+            {
+                OnInputDecision?.Invoke();
+                return;
+            }
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            if (h != 0 || v != 0)
+            {
+                OnInputArrow?.Invoke(h, v);
+            }
         }
     }
 }
