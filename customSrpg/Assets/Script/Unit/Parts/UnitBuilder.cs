9ff1c22d02087a53470d9b5516049955d820883c
sorude26 <sorude26@gmail.com>
2021-07-10 15:16:05 +0900

    ユニットの組み立てを行うスクリプトを追加した

---
+++ customSrpg/Assets/Script/Unit/Parts/UnitBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBuilder : MonoBehaviour
+{
+    [SerializeField] PartsHead head;
+    [SerializeField] PartsBody body;
+    [SerializeField] PartsArm rArm;
+    [SerializeField] PartsArm lArm;
+    [SerializeField] PartsLeg leg;
+    [SerializeField] Transform headP;
+    [SerializeField] Transform bodybP;
+    [SerializeField] Transform lArmbP;
+    [SerializeField] Transform lArm1P;
+    [SerializeField] Transform lArm2P;
+    [SerializeField] Transform rArmbP;
+    [SerializeField] Transform rArm1P;
+    [SerializeField] Transform rArm2P;
+    [SerializeField] Transform legbP;
+    [SerializeField] Transform lLegbP;
+    [SerializeField] Transform lLeg1P;
+    [SerializeField] Transform lLeg2P;
+    [SerializeField] Transform rLegbP;
+    [SerializeField] Transform rLeg1P;
+    [SerializeField] Transform rLeg2P;
+    public void UnitBuilde()
+    {
+
+    }
+}
