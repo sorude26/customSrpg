bd2d6d82e775e929e26aff387c50a62699063a3a
sorude26 <sorude26@gmail.com>
2021-07-12 01:32:01 +0900

    メッシュを生成するスクリプト

---
+++ customSrpg/Assets/Script/PartsMeshCreater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// メッシュを生成する
+/// </summary>
+public class PartsMeshCreater : MonoBehaviour
+{
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] string meshName = "default";
+   public void Create()
+    {
+        if (meshName == "")
+        {
+            Debug.LogError("ファイル名を入力してください");
+            return;
+        }
+        AssetDatabase.CreateAsset(meshFilter.mesh, "Assets/Material/Mesh/"+ meshName +".asset");
+        AssetDatabase.SaveAssets();
+    }
+}
