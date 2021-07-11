bb9078086243f03202be887f09f0876c5e46d8e0
sorude26 <sorude26@gmail.com>
2021-07-12 01:35:04 +0900

    コメントを追加した

--- customSrpg/Assets/Script/PartsMeshCreater.cs
+++ customSrpg/Assets/Script/PartsMeshCreater.cs
@@ -6,16 +6,21 @@ using UnityEditor;
 /// </summary>
 public class PartsMeshCreater : MonoBehaviour
 {
-    [SerializeField] MeshFilter meshFilter;
-    [SerializeField] string meshName = "default";
-   public void Create()
+    [Tooltip("メッシュを生成したいオブジェクト")]
+    [SerializeField] MeshFilter m_meshFilter;
+    [Tooltip("メッシュの名前")]
+    [SerializeField] string m_meshName = "default";
+    /// <summary>
+    /// メッシュを生成する
+    /// </summary>
+   public void CreateMesh()
     {
-        if (meshName == "")
+        if (m_meshName == "")
         {
             Debug.LogError("ファイル名を入力してください");
             return;
         }
-        AssetDatabase.CreateAsset(meshFilter.mesh, "Assets/Material/Mesh/"+ meshName +".asset");
+        AssetDatabase.CreateAsset(m_meshFilter.mesh, "Assets/Material/Mesh/"+ m_meshName +".asset");
         AssetDatabase.SaveAssets();
     }
 }
