a275b72b02167960502fe7a3ca2f07c3e3c2fe1a
sorude26 <sorude26@gmail.com>
2021-07-13 19:42:11 +0900

    パーツデータは別のシーンでも使用するため、GameManagerを作成し、パーツデータを持たせた

---
+++ customSrpg/Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instanse { get; private set; }
+    [SerializeField] UnitPartsList m_partsList;
+    public UnitPartsList PartsList { get => m_partsList; }
+    private void Awake()
+    {
+        if (Instanse)
+        {
+            Destroy(this);
+            return;
+        }
+        Instanse = this;
+    }
+}
