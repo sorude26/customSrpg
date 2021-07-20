74a39ad1c13e55287c2211b3d6335d75698c5403
sorude26 <sorude26@gmail.com>
2021-07-20 19:52:22 +0900

    格闘武器のスクリプトを仮作成し、専用武器を追加した

---
+++ customSrpg/Assets/Script/Unit/Parts/MeleeWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : WeaponMaster
+{
+    [SerializeField] Transform m_blade;
+    public override void AttackStart()
+    {
+        m_attackStart?.Invoke();
+        StartCoroutine(Attack());
+    }
+    IEnumerator Attack()
+    {
+        int count = m_partsData.MaxAttackNumber;
+        while (count > 0)
+        {
+            count--;
+            m_attack?.Invoke();
+            EffectManager.PlayEffect(EffectType.ShotHit, m_blade.position);
+            yield return new WaitForSeconds(m_partsData.AttackInterval);
+        }
+        m_attackEnd?.Invoke();
+        m_attackEnd = null;
+        m_attack = null;
+    }
+}
