75618386ee96ee9158dea15c974bf191f5786e67
sorude26 <sorude26@gmail.com>
2021-07-23 20:21:45 +0900

    パラメータ表示用のスクリプトを追加した

---
+++ customSrpg/Assets/Script/Stage/GaugeControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeControl : MonoBehaviour
+{
+    [SerializeField] Image m_hpGauge;
+    [SerializeField] Text m_hpText;
+
+    public int GaugeSet(int currentHp,int maxHp)
+    {
+        this.gameObject.SetActive(true);
+        if (currentHp < 0){ currentHp = 0; }
+        m_hpText.text = $"{currentHp}/{maxHp}";
+        m_hpGauge.fillAmount = (float)currentHp / maxHp;
+        return currentHp;
+    }
+    public void GaugeNone()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ customSrpg/Assets/Script/Stage/UnitDataGuideView.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitDataGuideView : MonoBehaviour
+{
+    [SerializeField] GaugeControl m_headGauge;
+    [SerializeField] GaugeControl m_bodyGauge;
+    [SerializeField] GaugeControl m_rArmGauge;
+    [SerializeField] GaugeControl m_lArmGauge;
+    [SerializeField] GaugeControl m_legGauge;
+    [SerializeField] GameObject[] m_partsView;
+    public void ViewData(Unit unit)
+    {
+        if (unit == null)
+        {
+            return;
+        }
+        gameObject.SetActive(true);
+        switch (unit.GetUnitData().Body.BodyPartsType)
+        {
+            case UnitType.Human:
+                DataSet(unit);
+                break;
+            case UnitType.Walker:
+                break;
+            case UnitType.Helicopter:
+                break;
+            case UnitType.Tank:
+                break;
+            default:
+                break;
+        }
+    }
+    void DataSet(Unit unit)
+    {
+        if (m_headGauge.GaugeSet(unit.GetUnitData().Head.ViewCurrentHp, unit.GetUnitData().Head.MaxPartsHp) > 0)
+            m_partsView[0].SetActive(true);
+        if (m_bodyGauge.GaugeSet(unit.GetUnitData().Body.ViewCurrentHp, unit.GetUnitData().Body.MaxPartsHp) > 0)
+            m_partsView[1].SetActive(true);
+        if (m_rArmGauge.GaugeSet(unit.GetUnitData().RArm.ViewCurrentHp, unit.GetUnitData().RArm.MaxPartsHp) > 0) 
+            m_partsView[2].SetActive(true);
+        if (m_lArmGauge.GaugeSet(unit.GetUnitData().LArm.ViewCurrentHp, unit.GetUnitData().LArm.MaxPartsHp) > 0)
+            m_partsView[3].SetActive(true);
+        if (m_legGauge.GaugeSet(unit.GetUnitData().Leg.ViewCurrentHp, unit.GetUnitData().Leg.MaxPartsHp) > 0)
+            m_partsView[4].SetActive(true);
+    }
+    public void ViewEnd()
+    {
+        foreach (var item in m_partsView)
+        {
+            item.SetActive(false);
+        }
+        gameObject.SetActive(false);
+    }
+}
