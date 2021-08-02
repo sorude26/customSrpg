4192ba2a6da381f15b75042f4177db44b528837f
sorude26 <sorude26@gmail.com>
2021-08-02 18:19:22 +0900

    JSONで扱うためのクラスを追加した

---
+++ customSrpg/Assets/Script/GameData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom
+{
+    /// <summary>
+    /// JSONで扱う為のクラス
+    /// </summary>
+    [System.Serializable]
+    public class GameData
+    {
+        public string unitHeadID;
+        public string unitBodyID;
+        public string unitRArmID;
+        public string unitLArmID;
+        public string unitLegID;
+        public string WeaponBodyID;
+        public string WeaponRArmID;
+        public string WeaponLArmID;
+    }
+}
