bd2dc4fed623b72236ee893f68176bc6e33dab73
sorude26 <sorude26@gmail.com>
2021-08-03 18:31:12 +0900

    セーブデータ関係のテストを行った

--- customSrpg/Assets/Script/GameData.cs
+++ customSrpg/Assets/Script/GameData.cs
@@ -10,13 +10,14 @@ namespace Custom
     [System.Serializable]
     public class GameData
     {
-        public string unitHeadID;
-        public string unitBodyID;
-        public string unitRArmID;
-        public string unitLArmID;
-        public string unitLegID;
+        public string UnitHeadID;
+        public string UnitBodyID;
+        public string UnitRArmID;
+        public string UnitLArmID;
+        public string UnitLegID;
         public string WeaponBodyID;
         public string WeaponRArmID;
         public string WeaponLArmID;
+        public string UnitColor;
     }
 }
---
+++ customSrpg/Assets/Script/GameDataManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Custom;
+/// <summary>
+/// セーブデータを扱うテスト
+/// </summary>
+public class GameDataManager : MonoBehaviour
+{
+    string m_dataPath;
+    private void Awake()
+    {
+        m_dataPath = Application.dataPath + "/JsonTest.json";
+    }
+    void Start()
+    {
+        GameData testData = new GameData();
+        string test = JsonUtility.ToJson(testData);
+        Debug.Log(test);
+    }
+
+    public void SaveData(GameData data)
+    {
+        string jsonData = JsonUtility.ToJson(data);
+        StreamWriter writer = new StreamWriter(m_dataPath, false);
+        writer.WriteLine(jsonData);
+        writer.Flush();
+        writer.Close();
+    }
+    public GameData LoadData(string dataPath)
+    {
+        StreamReader reader = new StreamReader(dataPath);
+        string data = reader.ReadToEnd();
+        reader.Close();
+        return JsonUtility.FromJson<GameData>(data);
+    }
+}
