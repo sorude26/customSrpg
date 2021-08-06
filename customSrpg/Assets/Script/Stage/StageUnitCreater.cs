6cc1a73d43dfe212656321bd091e4f08a131fde2
sorude26 <sorude26@gmail.com>
2021-08-06 16:35:05 +0900

    指定された座標に機体を生成するスクリプトを追加した

---
+++ customSrpg/Assets/Script/Stage/StageUnitCreater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ステージにユニットを生成する
+/// </summary>
+[CreateAssetMenu]
+public class StageUnitCreater : ScriptableObject
+{
+    [SerializeField] NpcUnit m_unitModelPrefab;
+    public NpcUnit[] StageUnitCreate(MapData[] positions, SortieUnits units,int start)
+    {
+        List<NpcUnit> unit = new List<NpcUnit>();
+        int i = start;       
+        for (int k = 0; k < units.SortiePattern.Length; k++)
+        {
+            for (int a = 0; a < units.SortiePattern[k]; a++)
+            {
+                if (i >= positions.Length)
+                {
+                    break;
+                }
+                unit.Add(CreateUnit(positions[i], units.BuildPattern[k], units.ColorPattern[k]));
+                i++;
+            }
+        }
+        return unit.ToArray();
+    }
+    NpcUnit CreateUnit(MapData pos,UnitBuildData buildData, Color color)
+    {
+        var unit = Instantiate(m_unitModelPrefab);
+        unit.StartSet(new Vector2Int(pos.PosX, pos.PosZ), buildData, color);
+        return unit;
+    }
+}
