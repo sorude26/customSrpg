4ba581558731b7b26ac4bbcf82bfdc3847f56868
sorude26 <sorude26@gmail.com>
2021-08-06 16:36:04 +0900

    ステージに出撃するユニットの仮パターンを作成した

--- customSrpg/Assets/Script/SortieUnits.cs
+++ customSrpg/Assets/Script/SortieUnits.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 /// <summary>
 /// 出撃する機体の編成設定
@@ -13,6 +12,8 @@ public class SortieUnits : ScriptableObject
     [SerializeField] Color[] m_unitsColorPattern;
     [Tooltip("出撃機体の構築パターン")]
     [SerializeField] UnitBuildData[] m_sortieUnitsBuildPattern;
+    /// <summary> 出撃機体の総数 </summary>
+    public int AllUnitNumber { get => m_sortieUnitsPattern.Sum(); }
     /// <summary> 出撃機体の編成数 </summary>
     public int[] SortiePattern { get => m_sortieUnitsPattern; }
     /// <summary> 出撃機体のカラー </summary>
