4cbfd1befd0c546f822b0922ba86ef282fb94536
sorude26 <sorude26@gmail.com>
2021-08-20 23:13:29 +0900

    コマンド用素材の追加とコマンドスクリプトを作成した

---
+++ customSrpg/Assets/Script/UI/CommandBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandBox : MonoBehaviour
+{
+    [Tooltip("コマンド名表示テキスト")]
+    [SerializeField] Text[] m_commandText;
+    [Tooltip("実行ボタン")]
+    [SerializeField] Button m_button;
+    /// <summary> クリック時のイベント </summary>
+    public event Action OnClickEvent;
+    /// <summary>
+    /// コマンド名設定
+    /// </summary>
+    /// <param name="command"></param>
+    public void SetText(string command)
+    {
+        for (int i = 0; i < m_commandText.Length; i++)
+        {
+            m_commandText[i].text = command;
+        }
+    }
+    /// <summary>
+    /// 選択時の処理
+    /// </summary>
+    public void OnClickCommand()
+    {
+        OnClickEvent?.Invoke();
+    }
+    public void SelectCommand()
+    {
+        m_button.Select();
+    }
+}
