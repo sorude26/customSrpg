3efebeebae6e0fa29927bbf3fb1948427f1a76fe
sorude26 <sorude26@gmail.com>
2021-08-31 18:57:10 +0900

    シーンを跨いでデータを管理するUnitBuildDataManagerを作成した

---
+++ customSrpg/Assets/Script/UnitBuildDataManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBuildDataManager
+{
+    const int AllUintCount = 8;
+    public int HaveUnitNumber { get; private set; } = 8;
+    public static UnitBuildData[] PlayerUnitBuildDatas { get; private set; } = new UnitBuildData[AllUintCount];
+    public static int[] PlayerColors { get; private set; } = new int[AllUintCount];
+    public void SetData(int number,UnitBuildData data,int color)
+    {
+        if (number >= AllUintCount || number < 0)
+        {
+            Debug.Log("指定対象は存在しません");
+            return;
+        }
+        PlayerUnitBuildDatas[number] = data;
+        PlayerColors[number] = color;
+    }
+}
