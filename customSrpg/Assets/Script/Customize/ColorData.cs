ce9158438d8bc3e21cef180dc37bdf477ca44cad
sorude26 <sorude26@gmail.com>
2021-09-03 23:15:43 +0900

    色のパターンを追加した

--- customSrpg/Assets/Script/Customize/ColorData.cs
+++ customSrpg/Assets/Script/Customize/ColorData.cs
@@ -7,18 +7,18 @@ public class ColorData : ScriptableObject
 {
     [SerializeField] byte[] m_colorPattern = { 20, 40, 63, 86, 109, 132, 155, 188, 211, 234, 247, 255 };
     public int PatternNum { get => m_colorPattern.Length; }
-    Vector3[] m_rgbPattern = {
-            new Vector3(1, 0, 0), new Vector3(1, 0.3f, 0), new Vector3(1, 0.5f, 0), new Vector3(1, 0.8f, 0), new Vector3(1, 1, 0), new Vector3(0.5f, 1, 0),
+    [SerializeField] Vector3[] m_rgbPattern = {
+            new Vector3(0.7f, 0.7f, 0.7f), new Vector3(1, 0, 0), new Vector3(1, 0.3f, 0), new Vector3(1, 0.5f, 0), new Vector3(1, 0.8f, 0), new Vector3(1, 1, 0), new Vector3(0.5f, 1, 0),
             new Vector3(0, 1, 0), new Vector3(0, 1, 0.5f), new Vector3(0, 1, 1), new Vector3(0, 0.8f, 1), new Vector3(0, 0.5f, 1), new Vector3(0, 0.2f, 1),new Vector3(0, 0, 1),
-            new Vector3(0.2f, 0, 1), new Vector3(0.5f, 0, 1), new Vector3(0.7f, 0, 1) , new Vector3(1, 0, 1), new Vector3(1, 0, 0.8f), new Vector3(1, 0, 0.5f), new Vector3(1, 1, 1) };
+            new Vector3(0.2f, 0, 1), new Vector3(0.5f, 0, 1), new Vector3(0.7f, 0, 1) , new Vector3(1, 0, 1), new Vector3(1, 0, 0.8f), new Vector3(1, 0, 0.5f) };
     public int ColorTypeNum { get => m_rgbPattern.Length; }
     public Color GetColor(int patternNum,int colorType)
     {
         if (patternNum >= m_colorPattern.Length)
         {
-            return new Color32((byte)((m_rgbPattern[colorType].x * m_colorPattern[PatternNum * 2 - patternNum - 1])),
-                         (byte)((m_rgbPattern[colorType].y * m_colorPattern[PatternNum * 2 - patternNum - 1] )),
-                         (byte)((m_rgbPattern[colorType].z * m_colorPattern[PatternNum * 2 - patternNum - 1] )), 255);
+            return new Color32((byte)(m_rgbPattern[colorType].x * 127 + m_colorPattern[PatternNum * 2 - patternNum - 1] * 0.5f),
+                         (byte)(m_rgbPattern[colorType].y * 127 + m_colorPattern[PatternNum * 2 - patternNum - 1] * 0.5f),
+                         (byte)(m_rgbPattern[colorType].z * 127 + m_colorPattern[PatternNum * 2 - patternNum - 1] * 0.5f), 255);
         }
         return new Color32((byte)(m_colorPattern[patternNum] * m_rgbPattern[colorType].x),
                          (byte)(m_colorPattern[patternNum] * m_rgbPattern[colorType].y),
