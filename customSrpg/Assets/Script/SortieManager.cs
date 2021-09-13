2849a302a2179207b619992e24687d56c732f0d1
sorude26 <sorude26@gmail.com>
2021-09-13 15:18:22 +0900

    出撃データの管理スクリプトを追加した

---
+++ customSrpg/Assets/Script/SortieManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortieManager
+{
+    public static int[] SoriteUnit { get; private set; }
+    public static int SoriteNumber { get; private set; }
+    public static int StageLevel { get; private set; }
+    public static int AlliesNumber { get; private set; }
+    public static int StageSizeLevel { get; private set; }
+    public static void SetStageData(int soriteNumber, int level, int alliesNumber)
+    {
+        SoriteNumber = soriteNumber;
+        StageLevel = level;
+        AlliesNumber = alliesNumber;
+    }
+    public static void SetPlayer(int[] sriteUnit)
+    {
+        SoriteUnit = sriteUnit;
+    }
+}
